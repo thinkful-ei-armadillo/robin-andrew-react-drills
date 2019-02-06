66e646fc1c08b8dad02e8b06ad08c2260368baec
Robin Khiv <robinkhiv@gmail.com>
2019-02-06 11:59:54 -0800

    Roulette

---
+++ src/state/Roulette.js
@@ -0,0 +1,35 @@
+import React,{ Component } from 'react';
+
+export default class Roulette extends Component{
+    // constructor(prop){
+    //     super(prop);
+    //     bulletInChamber= 8
+    // }
+
+    state = {
+        chamber: null,  
+        spinningTheChamber: false
+    }
+
+handleOutput= ()=>{
+
+}
+handleClick = () => {
+  this.setState({
+    spinningTheChamber: !this.state.spinningTheChamber});
+  let timeout = setTimeout(function(){
+    const mathstuff = Math.ceil(Math.random() * 8);
+    this.setState({ chamber: 1})}, 2000)
+}
+
+render() {
+    const bulletInChamber = 8;
+    return (
+    <div>
+        <p>{this.state.chamber}</p>
+        <button onClick={this.handleClick}>Pull The Trigger</button>
+    </div>
+)
+
+}
+}
