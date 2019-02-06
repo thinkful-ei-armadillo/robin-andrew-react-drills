f0fd14e3f2022fea0b381268058b18c2477ffa3d
Robin Khiv <robinkhiv@gmail.com>
2019-02-06 11:55:44 -0800

    bullet

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './state/HelloWorld';
 import Bomb from './state/Bomb';
+import Roulette from './state/Roulette'
 
 
-ReactDOM.render(<Bomb />, document.getElementById('root'));
+ReactDOM.render(<Roulette />, document.getElementById('root'));
 
 
--- src/state/Bomb.js
+++ src/state/Bomb.js
@@ -3,20 +3,29 @@ import React, { Component } from "react";
 class Bomb extends Component {
   state = {
     count: 0
-  };
-
+  }; 
+  interval = null;
+ 
   componentDidMount() {
-    setInterval(() => {
+    this.inverval = setInterval(() => {
       this.setState({ count: this.state.count + 1 });
     }, 1000);
   }
 
+  componentWillUnmount(){
+    clearInterval(this.interval)
+  }
+
   handleTickDisplay = () => {
     if (this.state.count === 8) {
+      clearInterval(this.interval)
       return "BOOM!";
-    } else if (this.state.count === 9) {
-      this.setState({ count: 0 });
-    } else if (this.state.count % 2 === 0) {
+    } else if(this.state.count === 9){
+      this.setState({
+        count: 0
+      })
+    } 
+    else if (this.state.count % 2 === 0) {
       return "tick";
     } else if (this.state.count % 2 !== 0) {
       return "tock";
