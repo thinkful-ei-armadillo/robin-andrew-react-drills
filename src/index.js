f1e580d3a94ac83e347b35b0df26d9f25df0a7e7
Robin Khiv <robinkhiv@gmail.com>
2019-02-06 13:10:58 -0800

    index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import HelloWorld from './state/HelloWorld';
 import Bomb from './state/Bomb';
 import Roulette from './state/Roulette'
+import Accordion from './state/Accordion';
+import sections from './state/STORE';
 
 
-ReactDOM.render(<Roulette />, document.getElementById('root'));
+ReactDOM.render(<Accordion sections = {sections}/>, document.getElementById('root'));
 
 
